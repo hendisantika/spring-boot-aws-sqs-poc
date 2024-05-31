91b4fc5dce1756b454959372d8f5232c5fe2f796
hendisantika <hendisantika@yahoo.co.id>
2024-05-31 15:14:57 +0700

    Add User Record🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/awssqspoc/model/User.java
@@ -0,0 +1,14 @@
+package id.my.hendisantika.awssqspoc.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-aws-sqs-poc
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/31/24
+ * Time: 15:12
+ * To change this template use File | Settings | File Templates.
+ */
+public record User(String id, String name, String email) {
+}
