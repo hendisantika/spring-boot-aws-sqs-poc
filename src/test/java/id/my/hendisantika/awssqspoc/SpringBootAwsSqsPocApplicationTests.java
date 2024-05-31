b0859ddb406a548491118adeefbb5bf154b89c95
hendisantika <hendisantika@yahoo.co.id>
2024-05-31 15:25:03 +0700

    Add givenAStringPayload_whenSend_shouldReceive🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/awssqspoc/SpringBootAwsSqsPocApplicationTests.java
+++ src/test/java/id/my/hendisantika/awssqspoc/SpringBootAwsSqsPocApplicationTests.java
@@ -3,18 +3,20 @@ package id.my.hendisantika.awssqspoc;
 import id.my.hendisantika.awssqspoc.config.EventQueuesProperties;
 import id.my.hendisantika.awssqspoc.repository.UserRepository;
 import io.awspring.cloud.sqs.operations.SqsTemplate;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.logging.Logger;
-import org.junit.platform.commons.logging.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.time.Duration;
+
+import static org.awaitility.Awaitility.await;
+
+@Slf4j
 @SpringBootTest
 @ActiveProfiles("introduction")
 class SpringBootAwsSqsPocApplicationTests extends BaseSqsLiveTest {
-
-    private static final Logger logger = LoggerFactory.getLogger(SpringBootAwsSqsPocApplicationTests.class);
 
     @Autowired
     private SqsTemplate sqsTemplate;
@@ -26,7 +28,19 @@ class SpringBootAwsSqsPocApplicationTests extends BaseSqsLiveTest {
     private EventQueuesProperties eventQueuesProperties;
 
     @Test
-    void contextLoads() {
+    void givenAStringPayload_whenSend_shouldReceive() {
+        // given
+        var userName = "Albert";
+
+        // when
+        sqsTemplate.send(to -> to.queue(eventQueuesProperties.getUserCreatedByNameQueue())
+                .payload(userName));
+        log.info("Message sent with payload {}", userName);
+
+        // then
+        await().atMost(Duration.ofSeconds(3))
+                .until(() -> userRepository.findByName(userName)
+                        .isPresent());
     }
 
 }
