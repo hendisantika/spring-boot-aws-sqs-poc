f77883477312e930e5243607c28584a3bd7af6ee
hendisantika <hendisantika@yahoo.co.id>
2024-05-31 15:23:52 +0700

    Modify SpringBootAwsSqsPocApplicationTests🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/awssqspoc/SpringBootAwsSqsPocApplicationTests.java
+++ src/test/java/id/my/hendisantika/awssqspoc/SpringBootAwsSqsPocApplicationTests.java
@@ -1,10 +1,29 @@
 package id.my.hendisantika.awssqspoc;
 
+import id.my.hendisantika.awssqspoc.config.EventQueuesProperties;
+import id.my.hendisantika.awssqspoc.repository.UserRepository;
+import io.awspring.cloud.sqs.operations.SqsTemplate;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class SpringBootAwsSqsPocApplicationTests {
+@ActiveProfiles("introduction")
+class SpringBootAwsSqsPocApplicationTests extends BaseSqsLiveTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(SpringBootAwsSqsPocApplicationTests.class);
+
+    @Autowired
+    private SqsTemplate sqsTemplate;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private EventQueuesProperties eventQueuesProperties;
 
     @Test
     void contextLoads() {
