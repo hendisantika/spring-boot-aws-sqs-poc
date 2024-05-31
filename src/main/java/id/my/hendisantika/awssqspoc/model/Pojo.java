43aa1c7da074106974e90ce908a14714dbe28ff1
hendisantika <hendisantika@yahoo.co.id>
2024-05-31 14:18:27 +0700

    Add Pojo Model 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/awssqspoc/model/Pojo.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.awssqspoc.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-aws-sqs-poc
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/31/24
+ * Time: 14:18
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pojo {
+    private String someValue;
+}
