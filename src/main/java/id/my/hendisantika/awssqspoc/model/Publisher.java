843c3c9855337d069055f801670cc6bb77928ce2
hendisantika <hendisantika@yahoo.co.id>
2024-05-31 14:24:50 +0700

    Add scheduleFixedRateTask in Publisher Model 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/awssqspoc/model/Publisher.java
+++ src/main/java/id/my/hendisantika/awssqspoc/model/Publisher.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
@@ -25,4 +26,11 @@ public class Publisher {
 
     @Value("${cloud.aws.end-point.uri}")
     private String endpoint;
+
+    @Scheduled(fixedRate = 1000)
+    public void scheduleFixedRateTask() {
+        log.info("Sending Message to SQS ");
+        //queueMessagingTemplate.send(endpoint, MessageBuilder.withPayload("Niraj").build());
+        queueMessagingTemplate.convertAndSend(endpoint, new Pojo("SomeRandomValue"));
+    }
 }
