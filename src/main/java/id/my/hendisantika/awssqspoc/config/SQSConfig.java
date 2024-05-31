8ebdbe6097dad8b898b8d1a961e9526f729cc4b3
hendisantika <hendisantika@yahoo.co.id>
2024-05-31 14:26:07 +0700

    Add amazonSQSAsync in SQSConfig 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/awssqspoc/config/SQSConfig.java
+++ src/main/java/id/my/hendisantika/awssqspoc/config/SQSConfig.java
@@ -1,9 +1,15 @@
 package id.my.hendisantika.awssqspoc.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 /**
  * Created by IntelliJ IDEA.
@@ -30,5 +36,13 @@ public class SQSConfig {
     @Bean
     public QueueMessagingTemplate queueMessagingTemplate() {
         return new QueueMessagingTemplate(amazonSQSAsync());
+    }
+
+    @Bean
+    @Primary
+    public AmazonSQSAsync amazonSQSAsync() {
+        return AmazonSQSAsyncClientBuilder.standard().withRegion(Regions.US_EAST_1)
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+                .build();
     }
 }
