4c94985424c1a9281718e0d170a32f151294b6da
hendisantika <hendisantika@yahoo.co.id>
2024-06-03 07:07:13 +0700

    Add customHeaderMessage in UserEventListeners 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/awssqspoc/introduction/UserEventListeners.java
+++ src/main/java/id/my/hendisantika/awssqspoc/introduction/UserEventListeners.java
@@ -3,9 +3,13 @@ package id.my.hendisantika.awssqspoc.introduction;
 import id.my.hendisantika.awssqspoc.repository.UserRepository;
 import io.awspring.cloud.sqs.annotation.SqsListener;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
+
+import static io.awspring.cloud.sqs.listener.SqsHeaders.MessageSystemAttributes.SQS_APPROXIMATE_FIRST_RECEIVE_TIMESTAMP;
 
 /**
  * Created by IntelliJ IDEA.
@@ -40,5 +44,13 @@ public class UserEventListeners {
     public void receiveRecordMessage(UserCreatedEvent event) {
         log.info("Received message: {}", event);
         userRepository.save(new User(event.id(), event.username(), event.email()));
+    }
+
+    @SqsListener("${events.queues.user-created-event-type-queue}")
+    public void customHeaderMessage(Message<UserCreatedEvent> message, @Header(EVENT_TYPE_CUSTOM_HEADER) String eventType,
+                                    @Header(SQS_APPROXIMATE_FIRST_RECEIVE_TIMESTAMP) Long firstReceive) {
+        log.info("Received message {} with event type {}. First received at approximately {}.", message, eventType, firstReceive);
+        UserCreatedEvent payload = message.getPayload();
+        userRepository.save(new User(payload.id(), payload.username(), payload.email()));
     }
 }
