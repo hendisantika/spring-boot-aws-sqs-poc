e986f9dcb35c15fafdf10177ed1ab15ceeb4d400
hendisantika <hendisantika@yahoo.co.id>
2024-06-03 07:23:25 +0700

    Add checkInventory in InventoryService 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/awssqspoc/acknowledgement/service/InventoryService.java
+++ src/main/java/id/my/hendisantika/awssqspoc/acknowledgement/service/InventoryService.java
@@ -1,6 +1,8 @@
 package id.my.hendisantika.awssqspoc.acknowledgement.service;
 
 import id.my.hendisantika.awssqspoc.acknowledgement.config.ProductIdProperties;
+import id.my.hendisantika.awssqspoc.acknowledgement.exception.OutOfStockException;
+import id.my.hendisantika.awssqspoc.acknowledgement.exception.ProductNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Service;
@@ -33,5 +35,20 @@ public class InventoryService implements InitializingBean {
         this.inventory = new ConcurrentHashMap<>(Map.of(productIdProperties.getSmartphone(), 10,
                 productIdProperties.getWirelessHeadphones(), 15,
                 productIdProperties.getLaptop(), 5));
+    }
+
+    public void checkInventory(UUID productId, int quantity) {
+        Integer stock = inventory.get(productId);
+        if (stock == null) {
+            throw new ProductNotFoundException("Product with id %s not found in Inventory".formatted(productId));
+        }
+        if (stock < quantity) {
+            // Simulate Stock Replenishment for Retries
+            inventory.put(productId, stock + (int) (Math.random() * 5));
+            throw new OutOfStockException(
+                    "Product with id %s is out of stock. Quantity requested: %s ".formatted(productId, quantity));
+        }
+        // Decrease inventory
+        inventory.put(productId, stock - quantity);
     }
 }
